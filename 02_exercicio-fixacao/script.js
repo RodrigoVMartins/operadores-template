c8c50dc4a6f0f0e595bc47fc0f3c1915ed1e331e
Rodrigo Vitor Martins Godoy <rodrigo.godoy4556@gmail.com>
2023-05-06 21:05:47 -0300

    Exercicios de fixação da aula de Operadores

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,11 @@
 // EXERCICIO DE FIXAÇÃO
 
+let suaIdade = Number(prompt("Qual sua idade? "))
+let idadeAmigo = Number(prompt("Qual a idade do seu ou de sua melhor amiga(o)? "))
+
+let comparacao = suaIdade > idadeAmigo
+let idadeMaior = "Sua idade é maior que a do seu amigo(a)?"
+console.log(idadeMaior, comparacao);
+
+let diferencaIdade = suaIdade - idadeAmigo
+console.log("A diferença de idade entre vocês é de", diferencaIdade)
